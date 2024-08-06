a3eeb951e228572bd446f6751c99aa845b18a0af
agos4567 <agosgalfre23@gmail.com>
2024-08-06 01:51:20 -0300

    Segunda-Parte historial Json

--- HistorialJson .cs
+++ HistorialJson .cs
@@ -9,7 +9,19 @@ namespace EspacioPersonaje
     {
 
 
-        
+        // GuardarGanador: Guarda el personaje ganador y la información relevante en un archivo JSON
+        public static void GuardarGanador(Personaje ganador, string nombreArchivo)
+        {
+            var ganadores = LeerGanadores(nombreArchivo) ?? new List<Personaje>();
+
+            ganadores.Add(ganador);
+
+            // Serializar la lista de ganadores en JSON y guardar en archivo
+            var opciones = new JsonSerializerOptions { WriteIndented = true };
+            string jsonString = JsonSerializer.Serialize(ganadores, opciones);
+            File.WriteAllText(nombreArchivo, jsonString);
+        }
+
  }
          
 }
