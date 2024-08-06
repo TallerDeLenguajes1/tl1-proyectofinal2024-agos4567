9fdc4b3e26418e5accbf890495db070f27a9bfb7
agos4567 <agosgalfre23@gmail.com>
2024-08-06 01:47:27 -0300

    trabajando con historialJson

---
+++ HistorialJson .cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+namespace EspacioPersonaje
+{
+    public class HistorialJson
+    {
+
+
+        
+ }
+         
+}
